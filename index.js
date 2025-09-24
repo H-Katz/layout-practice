e19ffa972dd352d59ffc284284ab2e4f3af9faf1
HATANO katsuhiko <hatano.katsuhiko@gmail.com>
2025-09-24 13:55:12 +0900

    final 43 classify

--- index.js
+++ index.js
@@ -16,20 +16,9 @@ class SVGCanvas {
         // 2. 最大・最小を求める
         // const minLon = Math.min(...lons), maxLon = Math.max(...lons);
         // const minLat = Math.min(...lats), maxLat = Math.max(...lats);
-        const lons = points.map((point) => point[0]);
-        const lats = points.map((point) => point[1]);
-        this.minLon = this.min(lons);
-        this.maxLon = this.max(lons);
-        this.minLat = this.min(lats);
-        this.maxLat = this.max(lats);
 
         // 3. 描画するSVG/Canvasのサイズを調整
-        const aspect =
-          (this.maxLat - this.minLat) / (this.maxLon - this.minLon);
-        this.width = this.root.getAttribute("width") - 0;
-        this.height = aspect * this.width;
-
-        this.root.setAttribute("height", Math.round(this.height));
+        this.resize(points);
 
         // 4. 緯度経度→SVG座標変換
         const svgCoordPolylines = this.toCanvasCoordFromPolylines(polylines);
@@ -40,6 +29,19 @@ class SVGCanvas {
         const pathNode = this.root.querySelector("path");
         pathNode.setAttribute("d", profile);
       });
+  }
+  resize(points){
+    const lons = points.map((point) => point[0]);
+    const lats = points.map((point) => point[1]);
+    this.minLon = this.min(lons);
+    this.maxLon = this.max(lons);
+    this.minLat = this.min(lats);
+    this.maxLat = this.max(lats);
+
+    const aspect = (this.maxLat - this.minLat) / (this.maxLon - this.minLon);
+    this.width = this.root.getAttribute("width") - 0;
+    this.height = aspect * this.width;
+    this.root.setAttribute("height", Math.round(this.height));  
   }
   min(ary) {
     return ary.reduce((a, b) => Math.min(a, b));
@@ -81,12 +83,12 @@ class SVGCanvas {
 var controller = {
   nextLocation: (evt) => {
     evt.preventDefault();
-    const i = Math.floor(Math.random() * cityOfficeLocations.length);
+    const i = Math.floor(Math.random() * selectedCities.length);
     controller.updateLocation(i);
-    document.forms.$cities.oita.value = cityOfficeLocations[i][0];
+    document.forms.$cities.oita.value = selectedCities[i][0];
   },
   updateLocation: (i) => {
-    const cityOffice = cityOfficeLocations[i];
+    const cityOffice = selectedCities[i];
     let lng = cityOffice[9] - 0;
     let lat = cityOffice[8] - 0;
     let [cx, cy] = svg.toCanvasCoordFromPoint([lng, lat]);
@@ -98,7 +100,7 @@ var controller = {
 
 var pickCity = (evt) => {
   const key = document.forms.$cities.oita.value;
-  const i = cityOfficeLocations.findIndex((city) => city[0] == key);
+  const i = selectedCities.findIndex((city) => city[0] == key);
   if (i > -1) {
     controller.updateLocation(i);
   }
@@ -110,9 +112,22 @@ var toggle = (evt) => {
   toggleNode.classList.toggle("filter", !checked);
 };
 
+Array.prototype.group_by = function(eval){
+  return this.reduce((acc, val)=>{
+    const key = eval(val);
+    if(acc[key] == null){
+      acc[key] = [val]
+    }else{
+      acc[key].push(val);
+    }
+    return acc;
+  }, {})
+}
+
 var svg = new SVGCanvas("profile");
 svg.load("./N03-21_44_210101.geojson");
 var cityOfficeLocations = null;
+var selectedCities = null;
 
 (async () => {
   cityOfficeLocations = await fetch("./r0612puboffice_utf8.csv")
@@ -127,11 +142,18 @@ var cityOfficeLocations = null;
       });
       console.log(cityOfficeLocations[0]);
 
+      /*
       cityOfficeLocations = cityOfficeLocations.filter((cityOffice) => {
         // return cityOffice[1] == "大分県"
         return cityOffice[0].startsWith("44") && cityOffice[0].length > 4;
       });
       console.log(cityOfficeLocations);
+      */
+      cityOfficeLocations.forEach((city)=>{
+        if(city[0].length < 5)
+          city[0] = "0" + city[0];
+      })
+      selectedCities = cityOfficeLocations;
 
       return cityOfficeLocations;
     });
@@ -190,4 +212,95 @@ cities = {"44000": "大分県",
   // ulを加える。どこに？ <h2>自治体</h2>の弟ノードにしたい
   let base = document.querySelector("aside h2:last-child");
   base.parentNode.appendChild(ul);
+})();
+
+(async () => {
+  const boundaries = await (await fetch("./data/N03-21_210101.json")).json()
+  let prefectureNames = boundaries.features.map((feature) => feature.properties["N03_001"]);
+  console.log("自治体の境界の数", prefectureNames.length)
+  prefectureNames = [...new Set(prefectureNames)]; // 手っ取り早く重複を削除
+  console.log("都道府県のリスト", prefectureNames);
+
+  const list = prefectureNames.map((value) => {
+    const option = document.createElement("option");
+    option.textContent = value;
+    return option;
+  });
+
+  const select = document.querySelector("select[name='prefecture']");
+  select.innerHTML = ""; // 初期化
+  list.reduce((root, option) => {
+    root.appendChild(option);
+    return root;
+  }, select);
+
+  select.addEventListener("change", (evt) => {
+    const value = evt.target.value;
+    // alert(value);
+    // 1. 選択した都道府県に含まれる幾何データを取得
+    const geometries = boundaries.features.filter((feature) => feature.properties["N03_001"] == value).map((feature)=>feature.geometry);
+
+    // 幾何データをポリラインに変換
+    // ポリゴンの場合は、ポリラインを1つしか持っていない
+    // マルチポリゴンの場合は、ポリラインを複数持っている
+    const polylines= geometries.map((geometry=>{
+      if(geometry.type == "Polygon"){
+        return geometry.coordinates;
+      }else if(geometry.type == "MultiPolygon"){
+        return geometry.coordinates[0];
+      }
+    })).flat(1) // 最後にポリライン集合として平坦化する
+
+    const points = polylines.flat(1); // 一旦、ポリライン集合を点集合に変換し、描画サイズを調整
+    svg.resize(points);
+    // ポリラインを描画座標に変換した後、svgに描画
+    const profile = svg.toCanvasCoordFromPolylines(polylines).map((polyline)=>svg.toPath(polyline)).join(" ");
+    const pathNode = svg.root.querySelector("path");
+    pathNode.setAttribute("d", profile);
+
+    // 2. 選択した都道府県の自治体コードたちN03_007の配列を作る
+    const features = boundaries.features.filter((feature)=>feature.properties["N03_001"] == value);
+    // feature.properties["N03_007"]が自治体コード
+    const groupedCities = cityOfficeLocations.group_by((location)=>location[0]);
+
+    // 3. この自治体コード配列の要素それぞれについて、庁舎データを参照し緯度経度データを取得する
+    let cities = features.map((feature)=>{
+      const cityCode = feature.properties["N03_007"];
+      const cityOfficeLocation = groupedCities[cityCode][0]
+      return cityOfficeLocation;
+    })
+    console.log(cities)
+    selectedCities = cities;
+
+  // 配列からリストを生成 ... 1対1の時は ... ?
+  const list = cities.map((city) => {
+    let value = city[0];
+    let name = city[1];
+    const li = document.createElement("li");
+    li.innerHTML =
+      '<label><input type="radio" name="oita" value="' +
+      value +
+      '" onchange="pickCity(event)">' +
+      name +      
+      "</label>";
+    return li;
+  });
+
+  // 配列をある一つの要素にするには...? 集約機能だからreduce を使う
+  const ul = document.createElement("ul");
+  ul.classList.add("filter");
+  list.reduce((root, li) => {
+    root.append(li);
+    return root;
+  }, ul);
+
+    // ulを加える。どこに？ <h2>自治体</h2>の弟ノードにしたい
+    
+    let base = document.querySelector("aside h2:last-of-type");
+    if(base.nextElementSibling != null)
+      base.parentNode.removeChild(base.nextElementSibling);
+    base.parentNode.appendChild(ul);
+
+  });
+
 })();
